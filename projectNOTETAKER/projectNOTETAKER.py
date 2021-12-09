551ae3f73cc915976b8a0ad15760a01bd8070bc1
tjschwartz7 <54190751+tjschwartz7@users.noreply.github.com>
2021-12-09 09:43:06 -0500

    Update projectNOTETAKER.py
    UPDATE V3 full release

--- projectNOTETAKER/projectNOTETAKER.py
+++ projectNOTETAKER/projectNOTETAKER.py
@@ -3,6 +3,8 @@ import getpass
 from time import sleep
 from datetime import date
 import collections
+import re
+import enum
 
 #All of this code in the screen_clear function was written by tutorialspoint.com
 print("OS: " + str(os.name))
@@ -77,6 +79,145 @@ def display_f3():
     with open(file_path3, 'r') as fp:
          print(fp.read())
 
+
+def graphing_calculator():
+    user_query = ""
+
+    screen_clear()
+    while(user_query.lower() != "exit"):
+        sy_stack = []
+        sy_queue = []
+        noNumUntilOp = False
+        print("Enter a query ('exit' to leave)")
+        user_query = input()
+
+     #Thanks to brilliant.org for the pseudocode
+            #1.  While there are tokens to be read:
+        for i in range(0, len(user_query), 1):
+            if(user_query.lower() == "exit"):
+                break
+            #2.  Read a token
+            c = user_query[i]
+            #3.  If it's a number add it to queue
+            if(c.isnumeric() and not noNumUntilOp):
+                str_int = ""
+                if(i+1 < len(user_query) and user_query[i+1].isnumeric()):
+                    str_int += c
+                    while(i+1 < len(user_query) and user_query[i+1].isnumeric()):
+                        i += 1
+                        c = user_query[i]
+                        str_int += c
+                        noNumUntilOp = True
+                else:
+                    str_int += c
+                sy_queue.append(str_int)
+                str_int = ""
+            #4.  If it's an operator
+            elif(c == '+' or c == '-' or c == '/' or c == '*' or c=='^'):
+                noNumUntilOp = False
+         #5.      While there's an operator on the top of the stack with greater precedence:
+                while(sy_stack and get_prec(peek_stack(sy_stack)) >= get_prec(c)):
+         #6.   Pop operators from the stack onto the output queue
+                    sy_queue.append(sy_stack.pop())
+         #7.               Push the current operator onto the stack
+                sy_stack.append(c)
+	     #8.        If it's a left bracket push it onto the stack  
+            elif(c=='('):
+                sy_stack.append(c)
+         #9.        If it's a right bracket
+            elif(c==')'):
+         #10.            While there's not a left bracket at the top of the stack:
+                while(peek_stack(sy_stack) != '('):
+         #11.                     Pop operators from the stack onto the output queue.
+                    sy_queue.append(sy_stack.pop())
+         #12.             Pop the left bracket from the stack and discard it
+                sy_stack.pop()
+	     #13. While there are operators on the stack, pop them to the queue
+        while(sy_stack):
+            sy_queue.append(sy_stack.pop())
+
+        print("Solution: ")
+        print("=" + str(postfix_solver(sy_queue)))
+
+def peek_stack(stack):
+    if stack:
+        return stack[-1]
+
+
+def postfix_solver(sy_queue):
+    hold1 = ""
+    hold2 = ""
+    stack = []
+    for i in range(len(sy_queue)):
+        print(stack)
+        c = sy_queue[i]
+        if(stack and (c == '+' or c=='-' or c=='/' or c=='*'or c=='^')):
+            hold1 = stack.pop()
+            hold2 = stack.pop()
+            stack.append(operation_solver(hold1, hold2, c))
+        elif(c.isnumeric()):
+            stack.append(int(c))
+
+    return peek_stack(stack)
+
+
+
+def operation_solver(val1, val2, op):
+    if(op == '+'):
+        return val2+val1
+    elif(op=='-'):
+        return val2-val1
+    elif(op=='*'):
+        return val2*val1
+    elif(op=='/'):
+        return val2/val1
+    elif(op=='^'):
+        return pow(val2, val1)
+
+
+
+
+
+
+        
+
+    
+
+
+	
+
+
+
+
+
+
+
+
+def get_prec(c):
+    if(c=='+' or c=='-'):
+        return 1
+    elif(c=='/' or c=='*'):
+        return 2
+    elif(c=='^'):
+        return 3
+    else:
+        return 0
+        
+    
+
+
+
+def enqueue(li):
+    li.append()
+    return li
+
+def dequeue(li):
+    li.pop(0)
+    return li
+
+
+
+
 filename3 = 'filename_information'
 
 file_path3 = r'C:\Users\\'+getpass.getuser() + '\Documents\\'+filename3+'.txt'
@@ -104,11 +245,13 @@ with open(file_path1, 'a') as fp:
 with open(file_path2, 'a') as fp:
      fp.write(str(date.today()) + "\n")
 while(done == False):
+    screen_clear()
     print("You are connected to " + filename1 + ".txt and " + filename2 + ".txt")
     print("Exit(-1):")
     print("Change files(0):")
     print("Write to "+filename1+"(1):")
     print("Write to "+filename2+"(2):")
+    print("Graphing Calculator(3):")
 
     choose = int(input())
     if(choose==-1):
@@ -131,7 +274,8 @@ while(done == False):
             print("-999 to exit")
             while(text_input != "-999"):
                 text_input=input()
-                fp.write(text_input + "\n")
+                if(text_input != "-999"):
+                    fp.write(text_input + "\n")
     elif(choose==2):
         text_content = ""
         screen_clear()
@@ -143,17 +287,9 @@ while(done == False):
             print("-999 to exit")
             while(text_input != "-999"):
                 text_input=input()
-                fp.write(text_input + "\n")
+                if(text_input != "-999"):
+                    fp.write(text_input + "\n")
+    elif(choose==3):
+        graphing_calculator()
 
 print("Program end")
-
-
-        
-
-    
-    
-
-
-
-
-
